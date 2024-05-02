0f2c771f8d2f56da44e471c02f61fbe6c8f4049f
J0K3RY-03 <jokery03@hotmail.com>
2024-05-02 18:08:19 +0200

    first setup file for the lateral menu

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Header from '@/src/header/Header';
+import Menu from '@/src/lateralMenu/Menu';
 
 export default function Home() {
   return (
     <main className={'flex flex-col m-auto max-w-4xl px-4 h-full'}>
       <Header/>
+        <Menu/>
     </main>
   );
 }
---
+++ src/lateralMenu/components/List.jsx
@@ -0,0 +1,11 @@
+import ItemList from './ItemList';
+
+const List = () => {
+    return (
+        <ul>
+            <ItemList/>
+        </ul>
+    )
+}
+
+export default List;
